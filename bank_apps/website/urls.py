726504af048f2c250dae33f18a494d5b409faf36
Bhumika <28661773@students.lincoln.ac.uk>
2024-03-16 23:23:10 +0000

    dashboard design and uploaded icons

--- bank_apps/website/urls.py
+++ bank_apps/website/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
     path('login/', views.login, name = 'login'),
     path('otp/', views.otp, name = 'otp'),
+    path('main/', views.main, name = 'main'),
     # path('signup1/', views.signup1, name = 'signup1'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
