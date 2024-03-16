2cb33885d373ddea844dbf113f775dbe5f88a964
Bhumika <28661773@students.lincoln.ac.uk>
2024-03-16 15:49:19 +0000

    otp design and navigation from login to otp

--- bank_apps/website/urls.py
+++ bank_apps/website/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
     path('login/', views.login, name = 'login'),
+    path('otp/', views.otp, name = 'otp'),
     # path('signup1/', views.signup1, name = 'signup1'),
 ]
--- bank_apps/website/views.py
+++ bank_apps/website/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'title': 'home'})
+    return render(request, 'login.html', {'title': 'home'})
 
 def about(request):
-    return render(request, 'about.html', {'title': 'about'})
+    return render(request, 'login.html', {'title': 'about'})
 
 def login(request):
     return render(request, 'login.html')
+
+def otp(request):
+    return render(request, 'otp.html')
 
 # def signup1(request):
 #    return render(request, 'signup1.html')
